c7d3ec659fd9e99131ec5898555d263ee27f10a1
ricardo <ricardoliveira50@hotmail.com>
2021-07-02 20:04:19 -0300

    Lista de temas adicionado

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,15 +11,38 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
-  constructor( private router: Router) { }
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
 
-  ngOnInit(){
+  constructor(private router: Router, private temaService: TemaService) { }
 
-    window.scroll(0,0)
+  ngOnInit() {
 
-    if(environment.token == '' ){
+    window.scroll(0, 0)
+
+    if (environment.token == '') {
       this.router.navigate(['/entrar'])
     }
+
+    this.temaService.refreshToken()
+
+    this.findAllTemas()
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp
+      alert('Tema cadastrado com sucesso')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
   }
 
 }
